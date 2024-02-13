d88c7b4c48f55e86605b0d8a5807d654f9e5929b
REXEIL <rexeilg06@gmail.com>
2024-02-13 19:27:16 +0800

    Activity1

---
+++ src/main/Activity1.kt
@@ -0,0 +1,15 @@
+
+fun main(){
+
+    print("Enter your First Name: ")
+    var first = readLine()
+    print("Enter your Middle Name: ")
+    var mi = readLine()
+    print("Enter your Last Name: ")
+    var lastName = readLine()
+    print("Enter your Age: ")
+    var age:int = 100
+    println($first $mi $lastName)
+
+
+}
---
+++ src/main/kotlin/Activity1.kt
@@ -0,0 +1,20 @@
+
+
+
+fun main(){
+for(i in 1..5) {
+    println("Enter your First Name: ")
+    var first = readLine()
+    println("Enter your Middle Name: ")
+    var mi = readLine()
+    println("Enter your Last Name: ")
+    var lastName = readLine()
+    println("Enter your Age: ")
+    var age = readLine()?.toInt() ?: 0
+
+
+    println(" $first $mi. $lastName")
+    println("$age")
+}
+
+}
